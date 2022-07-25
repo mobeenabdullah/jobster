f267010b01201aa93200a0a9a9eff62e2c08d0ba
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-26 03:41:48 +0500

    React Router Setup

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -23,7 +24,9 @@ const Landing = () => {
             probably haven't heard of them taxidermy semiotics truffaut bushwick
             pickled gastropub mustache messenger bag jianbing four dollar toast.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
