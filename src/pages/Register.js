c0ff32a95a1b1dceb09655c12fe58146b06b36a0
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-27 04:25:01 +0500

    Refactor User Slice

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -22,7 +22,6 @@ const Register = () => {
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(`${name}:${value}`);
     setValues({ ...values, [name]: value });
   };
 
