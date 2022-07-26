6a6dc3a80c4f0c733a7ac35217cf83cd045945c8
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-07-27 04:19:52 +0500

    Unauthorized - Logout User

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -48,7 +48,10 @@ export const updateUser = createAsyncThunk(
       });
       return resp.data;
     } catch (error) {
-      console.log(error.response);
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue("Unauthorized! Logging Out...");
+      }
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
@@ -122,6 +125,12 @@ const userSlice = createSlice({
     [updateUser.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      toast.success("Logout Successful!");
+      removeUserFromLocalStorage();
     },
   },
 });
