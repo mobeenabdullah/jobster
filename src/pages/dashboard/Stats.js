63d09559244443bb94f69a370713b6fa71635cf4
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-08-01 05:22:02 +0500

    Stats Page Structure

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,14 +1,25 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { StatsContainer, Loading, ChartsContainer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
 import { showStats } from "../../features/allJobs/allJobsSlice";
-
 const Stats = () => {
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(showStats());
+    // eslint-disable-next-line
   }, []);
-
-  return <div>Stats</div>;
+  if (isLoading) {
+    return <Loading center />;
+  }
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default Stats;
